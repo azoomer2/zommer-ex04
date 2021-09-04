1f02420aeae784ee9b96c520e0002b861c48e66c
Alexander <azoomer2@gmail.com>
2021-09-04 10:33:13 -0400

    Initial Push

---
+++ src/main/java/Solution04.java
@@ -0,0 +1,20 @@
+public class Solution04 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+    Psuedo Code
+
+    System.out prompt for 'noun'
+    System.in for 'noun'
+    System.out prompt for 'verb'
+    System.in for 'verb'
+    System.out prompt for 'adjective'
+    System.in for 'adjective'
+    System.out prompt for 'adverb'
+    System.in for 'adverb'
+    System.out with +
+*/
