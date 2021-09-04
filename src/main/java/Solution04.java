9c28b6f7dac7115b609b9c1f50d82bd142eb3c7b
Alexander <azoomer2@gmail.com>
2021-09-04 10:43:58 -0400

    Full Code

--- src/main/java/Solution04.java
+++ src/main/java/Solution04.java
@@ -1,10 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Alexander Zommer
+ */
+
+import java.util.Scanner;
+
 public class Solution04 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        String noun = scan.nextLine();
+        System.out.print("Enter a verb: ");
+        String verb = scan.nextLine();
+        System.out.print("Enter an adjective: ");
+        String adjective = scan.nextLine();
+        System.out.print("Enter an adverb: ");
+        String adverb = scan.nextLine();
+        System.out.println("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
 
     }
 }
-
-
 /*
     Psuedo Code
 
